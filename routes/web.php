58af3c62db96ac5fea9bd6b4a193287754fd1a3c
rachel <hai30@163.com>
2018-06-28 23:16:48 +0800

    用户界面-商品列表

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', 'pagesController@root')->name('root');
-
+Route::redirect('/', '/products')->name('root');
+Route::get('/products', 'ProductsController@index')->name('products.index');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
